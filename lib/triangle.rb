3668bbfff4cdeb65197c5d0c592c06d4fb944d8a
github username <chao.alexander.123@gmail.com>
2018-10-15 15:27:19 -0400

    create methods to return kind of triangle and raise error if invalid

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && a == c
+      return :equilateral
+    elsif a == b || a == c || b == c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b) > c, (a + c) > b, (b + c) > a, a > 0, b > 0, c > 0]
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
 end
